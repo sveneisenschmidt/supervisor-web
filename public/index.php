dc5bd5e4064e2c06baaee4efae56c84547e17e9d
Sven Eisenschmidt <sveneisenschmidt@users.noreply.github.com>
2015-07-13 16:24:40 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../src/SE/Supervisor/Web/Resources/views',
-    'twig.options' => ['debug' => $app['debug']]
+    'twig.options' => array('debug' => $app['debug'])
 ));
 $app->register(new Igorw\Silex\ConfigServiceProvider(
     __DIR__."/../config.json"
